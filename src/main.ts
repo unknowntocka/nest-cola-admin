1728342560f699de5231593dc90d725383e9652a
unknowntocka <1067075323@qq.com>
2023-03-31 16:50:24 +0800

    feat: 文档设置

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { generateDocument } from './app.doc';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 接入文档
+  generateDocument(app);
+
   await app.listen(3000);
 }
 bootstrap();
