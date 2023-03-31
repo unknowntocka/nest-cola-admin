87ad1b28d47ceb7de080e6b21e05cd63f705afc2
unknowntocka <1067075323@qq.com>
2023-03-31 18:42:47 +0800

    feat: 连接数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TYPEORM_CONFIG } from './common/constant';
+import database from './common/database';
 import { UserModule } from './user/user.module';
+import { getConfig } from './utils';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return configService.get<TypeOrmModuleOptions>(TYPEORM_CONFIG);
+      },
+    }),
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      isGlobal: true,
+      load: [getConfig, database],
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,14 @@
 import { readFileSync } from 'fs';
-import path from 'path';
+import { join } from 'path';
 import { parse } from 'yaml';
 
 export const getEnv = () => {
   return process.env.NODE_ENV;
 };
 
-export const getConfig = (property: string) => {
+export const getConfig = (property?: string) => {
   const env = getEnv();
-  const filePath = path.join(process.cwd(), `./.config/${env}.yaml`);
+  const filePath = join(process.cwd(), `./.config/${env}.yaml`);
   const file = readFileSync(filePath, 'utf8');
   const config = parse(file);
   return property ? config[property] : config;
